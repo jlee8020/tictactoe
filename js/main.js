e374018d070ac04e07fe549f16b0b49bf66e81a0
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-02-01 09:48:04 -0600

    initial placements

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/*------Constants------*/
+
+
+
+/*------Variables (state)------*/
+
+// Variables might include (board/turn/winner)
+
+/*------Cached Element References------*/
+
+// You might choose to put your game status here
+
+/*------Event Listeners------*/
+
+// This is where you should put the event listener
+// for a mouse-click
+
+/*------Functions------*/
+
+
+// Some functions you might choose to use:
+
+// Initialization function:
+// Where you set your initial state, setting up 
+// what the board will look like upon loading
+
+// On-Click function:
+// Set up what happens when one of the elements
+// is clicked
+
+
+// Check winner function:
+// Checks the current state of the board for
+// a winner and changes the state of the winner
+// variable if so
+
+
+// Render function:
+// Displays the current state of the board
+// on the page, updating the elements to reflect
+// either X or O depending on whose turn it is
+
+//TIC TAC TOE 
+
+// Pseudocode
+
+// This program will have 2 players. 
+// 	Player 1 - User Input
+// 	Player 2 - AI
+// Player 1 chooses between X or O
+// 	If user chooses O, AI will be assigned X
+// 	If user chooses X then AI will be assigned O
+// Initial message: “Please choose between button 1 or button 2!”
+// Initialize board
+// Message - Good luck!
+	
+// Players will take turns playing
+// 	Player 1 Input First
+// 	AI responds
+// 	Player 1 goes again
+// 	AI responds…. Etc
+
+// There will be 9 Total turns.
+// First player to assign/pick 3 containers in a row diagonally or horizontally wins!
+// Message: “Tic tac toe 3 in a Row!
+
+// Game Over
+// Play again
+// Reset/Break
