b0b906fa2004ccc9504df100baf054e0fff8eb8f
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-02-05 14:35:02 -0600

    1 last step needed

--- js/main.js
+++ js/main.js
@@ -1,24 +1,19 @@
 
 // /*----- constants -----*/
 
-const colors = {
-    'null' : 'blue',
-    'X': 'purple',
-    'O' : 'black',
-};
-
-
 // /*----- app's state (variables) -----*/
 let board;
 let turn;
 let winner;
 let playerOne;
 let playerTwo;
+let turnCount;
 
 
 /*----- cached element references -----*/
 let squares = document.querySelectorAll('div');
 let message = document.querySelector('h2');
+
 
 /*----- event listeners -----*/
 document.getElementById('x').addEventListener('click',handleChoiceClick);
@@ -31,69 +26,73 @@ init();
 function init(){
   board = [null, null, null, null, null, null, null, null, null];
   turn = 1;
+  turnCount = 1;
 }
 
 function handleChoiceClick(evt){
   playerOne = evt.target.textContent;
   if( playerOne === 'X'){
-         
-         playerTwo = 'O'; 
-         }
-      else { 
-        playerTwo = 'X'
-      
-      }
+         playerTwo = 'O'; }
+    else { 
+        playerTwo = 'X'}
   if(playerOne !== 'replay'){
-  message.textContent = `${playerOne} Hugs ${playerOne} goes first!`
+  message.textContent = `${playerOne} goes first!`
   }
   else{
-  message.textContent = `${playerTwo} Kisses ${playerTwo} goes second!`};
-
+  message.textContent = `${playerTwo} goes second!`};
 };
 
 init();
- 
+
 function markBoard(evt){
   let idx = parseInt(evt.target.id.replace('sq',''));
   if (board[idx] !== null || winner) {
     return evt;
   }
   if (turn === 1) {
-   evt.target.textContent = playerOne;
+    evt.target.textContent = playerOne;
     board[idx]= 1;}
-  else{
-    evt.target.textContent= playerTwo;
-    board[idx]= -1;
+    else{
+      evt.target.textContent= playerTwo;
+      board[idx]= -1;
     }
-  
     turn *= -1;
     getWinner();
+    turnCount++;
+    
+    if (turnCount === 10 && winner === false){
+      message.textContent = `This is a tie`}
+    return winner;
   }
+  
 
-
-
-function getWinner (){
-if(board[0] + board[1] + board[2] === 3 ||
-   board[3] + board[4] + board[5] === 3 ||
-   board[6] + board[7] + board[8] === 3 ||
-   board[0] + board[3] + board[6] === 3 ||
-   board[1] + board[4] + board[7] === 3 ||
-   board[2] + board[5] + board[8] === 3 ||
-   board[0] + board[4] + board[8] === 3 ||
-   board[2] + board[4] + board[6] === 3){
-    winner = true;
-    message.textContent = `${playerOne} Hugs ${playerOne} Wins!`
-   
-  }
-  if(board[0] + board[1] + board[2] === -3 ||
-    board[3] + board[4] + board[5] === -3 ||
-    board[6] + board[7] + board[8] === -3 ||
-    board[0] + board[3] + board[6] === -3 ||
-    board[1] + board[4] + board[7] === -3 ||
-    board[2] + board[5] + board[8] === -3 ||
-    board[0] + board[4] + board[8] === -3 ||
-    board[2] + board[4] + board[6] === -3){
-    winner = true;
-    message.innerHTML = `${playerTwo} Kisses ${playerTwo} Wins!`
-  };
-};
+  function getWinner (){
+    
+    if(board[0] + board[1] + board[2] === 3 ||
+      board[3] + board[4] + board[5] === 3 ||
+      board[6] + board[7] + board[8] === 3 ||
+      board[0] + board[3] + board[6] === 3 ||
+      board[1] + board[4] + board[7] === 3 ||
+      board[2] + board[5] + board[8] === 3 ||
+      board[0] + board[4] + board[8] === 3 ||
+      board[2] + board[4] + board[6] === 3){
+        winner = true;
+        message.textContent = `${playerOne} Wins!`
+        
+      }
+      if(board[0] + board[1] + board[2] === -3 ||
+        board[3] + board[4] + board[5] === -3 ||
+        board[6] + board[7] + board[8] === -3 ||
+        board[0] + board[3] + board[6] === -3 ||
+        board[1] + board[4] + board[7] === -3 ||
+        board[2] + board[5] + board[8] === -3 ||
+        board[0] + board[4] + board[8] === -3 ||
+        board[2] + board[4] + board[6] === -3){
+          winner = true;
+          message.innerHTML = `${playerTwo} Wins!`
+        };  
+      };
+      
+    
+      
+    
